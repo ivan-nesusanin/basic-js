5473591740d7bf6c193fb2af9060c7b74e486514
ivan-nesusanin <l75x5@yandex.ru>
2021-09-29 20:08:39 +0300

    feat: add st-matrix-elements-sum

--- src/st-matrix-elements-sum.js
+++ src/st-matrix-elements-sum.js
@@ -1,3 +1,6 @@
+/* Given matrix, a rectangular matrix of integers, just add up all the values that don't appear below a "0".
+]; */
+
 import { NotImplementedError } from '../extensions/index.js';
 
 /**
@@ -16,7 +19,25 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be 9
  */
-export default function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getMatrixElementsSum(matrix) {
+  // throw new NotImplementedError('Not implemented');
+  if (matrix.length === 0) return 0;
+  let arrOfZero = [];
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (!arrOfZero.includes(j)) {
+        if (matrix[i][j] === 0) {
+          arrOfZero.push(j)
+        } else {
+          sum += matrix[i][j];
+        }
+      } else {
+        if (matrix[i][j] !== 0) {
+          arrOfZero.shift(j);
+        }
+      }
+    }
+  }
+  return sum;
 }
